b81dbd044ad0a04b01af207824e5f23f1851655e
KevinPerezPijoUNMSM <kevingersy.perez@unmsm.edu.pe>
2024-07-03 01:09:24 -0500

    Agregando el toolbar en el activity main

--- app/src/main/java/com/stonic/stonic_erp_movil_fe/MainActivity.kt
+++ app/src/main/java/com/stonic/stonic_erp_movil_fe/MainActivity.kt
@@ -1,20 +1,64 @@
 package com.stonic.stonic_erp_movil_fe
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_sales, R.id.navigation_statistics, R.id.navigation_clients, R.id.navigation_storage))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_sales -> {
+                    toolbar.title = "Venta"
+                    toolbar.menu.clear()
+                    toolbar.inflateMenu(R.menu.menu_sales)
+                }
+                R.id.navigation_statistics -> {
+                    toolbar.title = "Estadística"
+                    toolbar.menu.clear()
+                    toolbar.inflateMenu(R.menu.menu_statistics)
+                }
+                R.id.navigation_clients -> {
+                    toolbar.title = "Clientes"
+                    toolbar.menu.clear()
+                    toolbar.inflateMenu(R.menu.menu_clients)
+                }
+                R.id.navigation_storage -> {
+                    toolbar.title = "Almacén"
+                    toolbar.menu.clear()
+                    toolbar.inflateMenu(R.menu.menu_storages)
+                }
+                else -> {
+                    toolbar.title = getString(R.string.app_name)
+                    toolbar.menu.clear()
+                }
+            }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
